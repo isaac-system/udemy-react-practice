41aa18a823ddf379255e241a9509e36dc5bc8540
isaac233@naver.com <isaac233@naver.com>
2023-01-09 19:13:16 +0900

    2023-01-09

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -12,7 +12,7 @@ const ExpenseItem = (props) => {
   */
   const [title, setTitle] = useState(props.title);
   // 새로운 값을 할당하는데 왜 const를 사용하는가?
-  //
+  // ???
   console.log("ExpenseItem evaluated by React");
   // 4 번 반복됨 why? Expense에서 컴포넌트의 독립적인 4개의 인스턴스가 생성되기 때문에
   // 그러나 clickHanlder 가 작동할 떄 1번만 나타남 why ? 위에 말했듯이 독립적인 하나의 인스턴스만 updated 되기 때문에
@@ -20,8 +20,9 @@ const ExpenseItem = (props) => {
 
   const clickHandler = () => {
     setTitle("Updated!!");
-    // state의 값을 변경할 떄 그 인스터스가 다시 rendering 된단.
+    // state의 값을 변경할 떄 그 인스터스가 다시 rendering 된다.
   };
+
   // function clickHandler() {}
 
   return (
@@ -31,6 +32,13 @@ const ExpenseItem = (props) => {
         <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      {/* 
+      clickHandler()가 아니라  clickHandler 라는 포인터를 전달한다.
+      why ?
+      {clickHandler()} 
+      클릭 했을 때 clickHandler가 실행되는 것이 아닌
+      이 JSX 코드가 재평가 되었을 때 실행된다.
+      */}
       <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
